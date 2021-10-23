1dfd027b4b7101c22c4bda3fa1abd9cf1e3cc0f1
Patrik <patrik@vaffel.xyz>
2021-10-23 19:24:23 +0200

    Fikset btrfs mount

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -90,11 +90,11 @@ esac
 # mount target
 mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@ -L ROOT /mnt
 mkdir /mnt/{boot,home,var,opt,tmp,.snapshots}
-mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@home ROOT /mnt/home
-mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@opt ROOT /mnt/opt
-mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@tmp ROOT /mnt/tmp
-mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@.snapshots ROOT /mnt/.snapshots
-mount -t btrfs -o subvol=@var /dev/sda3 /mnt/var
+mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@home -L ROOT /mnt/home
+mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@opt -L ROOT /mnt/opt
+mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@tmp -L ROOT /mnt/tmp
+mount -t btrfs -o noatime,commit=120,compress=zstd,space_cache,subvol=@.snapshots -L ROOT /mnt/.snapshots
+mount -t btrfs -o subvol=@var -L ROOT /mnt/var
 #mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount -t vfat -L UEFISYS /mnt/boot/
