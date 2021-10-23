0c8672649ec13151df31a59ce31f4be6f5128d18
Patrik <patrik.aakerholt@jarlsberg-ikt.no>
2021-10-23 16:11:22 +0200

    ddd

--- 1-setup.sh
+++ 1-setup.sh
@@ -29,16 +29,22 @@ echo "Changing the compression settings for "$nc" cores."
 sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
 
 echo "-------------------------------------------------"
-echo "       Setup Language to US and set locale       "
+echo "       Setup Language and set locale       "
 echo "-------------------------------------------------"
+
+echo "Please enter your timezone: (example Europe/Oslo)"
+read TZ
+
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone America/Chicago
+timedatectl --no-ask-password set-timezone ${TZ}
 timedatectl --no-ask-password set-ntp 1
 localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
 
 # Set keymaps
-localectl --no-ask-password set-keymap us
+echo "Please enter your keymap: (example no or us)"
+read KM
+localectl --no-ask-password set-keymap ${KM}
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
@@ -88,6 +94,7 @@ PKGS=(
 'efibootmgr' # EFI boot
 'egl-wayland'
 'exfat-utils'
+'firefox'
 'flex'
 'fuse2'
 'fuse3'
